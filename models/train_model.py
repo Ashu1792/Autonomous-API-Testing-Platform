5c8905d4839473eb77237ea6f15af33b0ea1ed46
Ashu1792 <ashupal112200@gmail.com>
2026-04-01 01:16:22 +0530

    upgrade css of login,register, and dashboard style

--- models/train_model.py
+++ models/train_model.py
@@ -1,34 +1,9 @@
 import sqlite3
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
-
-
-# connect database
-conn = sqlite3.connect("data/api_logs.db")
-
-df = pd.read_sql_query("SELECT * FROM logs", conn)
-
-conn.close()
-
-# create failure column
-df["failure"] = df["status_code"].apply(lambda x: 1 if x != 200 else 0)
-
-X = df[["response_time"]]
-y = df["failure"]
-
-model = LogisticRegression()
-
-model.fit(X, y)
-
-# save model
-pickle.dump(model, open("models/failure_model.pkl", "wb"))
-
-print("Model trained successfully")
 
 def train_model():
 
@@ -43,18 +18,18 @@ def train_model():
 
     print("Total rows:", len(df))
 
-    # ✅ Create target
+    # ✅ Target column
     df["failure"] = df["status_code"].apply(lambda x: 1 if x != 200 else 0)
 
     print("Class distribution:")
     print(df["failure"].value_counts())
 
-    # ❗ IMPORTANT CHECK
+    # ❗ Check if both classes exist
     if len(df["failure"].unique()) < 2:
         print("Only one class found!")
         return None, 0
 
-    # ✅ Convert time
+    # ✅ Feature engineering
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     df["hour"] = df["timestamp"].dt.hour
     df["minute"] = df["timestamp"].dt.minute
@@ -63,21 +38,24 @@ def train_model():
     X = df[["response_time", "hour", "minute"]]
     y = df["failure"]
 
-    # ✅ Split
+    # ✅ Train-test split
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    # ✅ Train
+    # ✅ Model
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
 
-    # ✅ Predict
+    # ✅ Prediction
     y_pred = model.predict(X_test)
 
     # ✅ Accuracy
     accuracy = accuracy_score(y_test, y_pred)
-
     print("Model Accuracy:", accuracy)
+
+    # ✅ Confusion Matrix
+    cm = confusion_matrix(y_test, y_pred)
+    print("Confusion Matrix:\n", cm)
 
     return model, accuracy
